6fda72a45f977d405885cba0fb3e94e48edaa94c
Leandro <leandrobrazao@outlook.com>
2018-08-21 00:41:53 -0300

    Con boton para sacar foto y grabar matriz

--- CamaraWebSencilla/CamaraWebSencilla/MainWindow.xaml.cs
+++ CamaraWebSencilla/CamaraWebSencilla/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace CamaraWebSencilla
 
         KinectSensor miKinect;
         short[] datosDistancia = null;
+        public int[][] mapaProfundidad = new int[480][];
+        public string[][] mapaBlanco = new string[480][];
 
         public MainWindow()
         {
@@ -34,8 +36,8 @@ namespace CamaraWebSencilla
         {
             miKinect = KinectSensor.KinectSensors.FirstOrDefault();
             miKinect.Start();
-            miKinect.ColorStream.Enable();
             miKinect.DepthStream.Enable();
+            miKinect.ColorStream.Enable();
             miKinect.DepthFrameReady += MiKinect_DepthFrameReady;
             miKinect.ColorFrameReady += miKinect_ColorFrameReady;
             
@@ -69,19 +71,85 @@ namespace CamaraWebSencilla
                 int largoDatosColor = datosColor.Length;
                 int i = 0;
                 bool encontro = false;
-                int blanco = 230;
-                while (i< largoDatosColor && !encontro && (i/2)<largoDatosColor)
+                int blanco = 254;
+                int k = 0, j = 0;
+                mapaProfundidad[j] = new int[640];
+                mapaBlanco[j] = new string[640];
+                i = 0;
+                while (i< largoDatosColor)
                 {
-                    if(datosColor[i] >= blanco && datosColor[i+1] >= blanco && datosColor[i+2] >= blanco)
+                    mapaBlanco[j][k] = ""; //j=480 x k=640
+                    if (datosColor[i] >= blanco && datosColor[i+1] >= blanco && datosColor[i+2] >= blanco)
                     {
                         int x = (i / 4) / 640;
                         int y = (i / 4 ) % 640;
-                        int z = datosDistancia[i / 4] >> 3;
-                        Console.WriteLine("BLANCO x: "+ x + ", y: " + y + ", z: " + z);
+                        int z = 0;
+                        try
+                        {
+                             z = datosDistancia[(i / 4)+25] >> 3;
+                        }
+                        catch {
+                            z = -9;
+                        }
+                        
+                        mapaBlanco[j][k] = "B"; //j=480 x k=640
+                        if(encontro == false)
+                        {
+                            Console.WriteLine("BLANCO x: " + x + ", y: " + y + ", z: " + z);
+                        }
                         encontro = true;
                     }
+                    k++;
+                    if (((i/4) + 1) % 640 == 0)
+                    {
+                        j++;
+                        if (j != 480)
+                        {
+                            mapaBlanco[j] = new string[640];
+                        }
+                        k = 0;
+                    }
+                    if (j % 480 == 0)
+                    {
+                        j = 0;
+                    }
+                    //Fin Calculo de j,k
                     i += 4;
                 }
+                j = 0;
+                k = 0;
+                i = 0;
+                while (i < (largoDatosColor / 4))
+                {
+                    //Calculo de j,k
+                    int valorDistancia = 0;
+                    try
+                    {
+                        valorDistancia = datosDistancia[i] >> 3;
+                    }
+                    catch
+                    {
+                        valorDistancia = -9;
+                    }
+                    mapaProfundidad[j][k] = valorDistancia; //j=480 x k=640
+                    k++;
+                    if ((i + 1) % 640 == 0)
+                    {
+                        j++;
+                        if (j != 480)
+                        {
+                            mapaProfundidad[j] = new int[640];
+                        }
+                        k = 0;
+                    }
+                    if (j % 480 == 0)
+                    {
+                        j = 0;
+                    }
+                    //Fin Calculo de j,k
+                    i++;
+                }
+
                 mostrarVideo.Source = BitmapSource.Create(
                     frameImagen.Width, frameImagen.Height,
                     96,
@@ -91,6 +159,34 @@ namespace CamaraWebSencilla
                     datosColor,
                     frameImagen.Width * frameImagen.BytesPerPixel
                     );
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int j = 0;
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"Mapa.txt", false))
+            {
+                while (j < 480)
+                {
+                    string linea = "";
+                    for (int i = 0; i < 640; i++)
+                    {
+                        if (mapaBlanco[j][i] == "B")
+                        {
+
+                            linea += "B " + Math.Abs(mapaProfundidad[j][i]).ToString("0000") + "  ";
+                        }
+                        else
+                        {
+                            linea += "X " + Math.Abs(mapaProfundidad[j][i]).ToString("0000") + "  ";
+                        }
+
+
+                    }
+                    file.WriteLine(linea);
+                    j++;
+                }
             }
         }
     }
